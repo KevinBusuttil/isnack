da277584241adf31f94e360f8948754f895ec353
Kevin Busuttil <kevin.busuttil@busuttil-technologies.com>
2025-11-10 17:07:29 +0100

    After upgrade of version 15 changed monkey_patch custom_conver_to_presentation_currency

--- isnack/monkey_patches/gl_currency.py
+++ isnack/monkey_patches/gl_currency.py
@@ -1,34 +1,78 @@
+import frappe
 from frappe.utils import flt
 from erpnext.accounts.report.utils import convert
 
-def custom_convert_to_presentation_currency(gl_entries, currency_info):
-    converted_gl_list = []
-    presentation_currency = currency_info["presentation_currency"]
-    company_currency = currency_info["company_currency"]
+# def custom_convert_to_presentation_currency(gl_entries, currency_info, filters=None):
+#     converted_gl_list = []
+#     presentation_currency = currency_info["presentation_currency"]
+#     company_currency = currency_info["company_currency"]
 
-    account_currencies = list(set(entry["account_currency"] for entry in gl_entries))
+#     account_currencies = list(set(entry["account_currency"] for entry in gl_entries))
 
-    for entry in gl_entries:
-        debit = flt(entry["debit"])
-        credit = flt(entry["credit"])
-        debit_in_account_currency = flt(entry["debit_in_account_currency"])
-        credit_in_account_currency = flt(entry["credit_in_account_currency"])
-        account_currency = entry["account_currency"]
+#     for entry in gl_entries:
+#         debit = flt(entry["debit"])
+#         credit = flt(entry["credit"])
+#         debit_in_account_currency = flt(entry["debit_in_account_currency"])
+#         credit_in_account_currency = flt(entry["credit_in_account_currency"])
+#         account_currency = entry["account_currency"]
 
-        if len(account_currencies) == 1 and account_currency == presentation_currency:
-            entry["debit"] = debit_in_account_currency
-            entry["credit"] = credit_in_account_currency
-        else:
-            # Use each entry's posting date for conversion
-            date = entry.get("posting_date") or currency_info["report_date"]
-            converted_debit_value = convert(debit, presentation_currency, company_currency, date)
-            converted_credit_value = convert(credit, presentation_currency, company_currency, date)
+#         if len(account_currencies) == 1 and account_currency == presentation_currency:
+#             entry["debit"] = debit_in_account_currency
+#             entry["credit"] = credit_in_account_currency
+#         else:
+#             # Use each entry's posting date for conversion
+#             date = entry.get("posting_date") or currency_info["report_date"]
+#             converted_debit_value = convert(debit, presentation_currency, company_currency, date)
+#             converted_credit_value = convert(credit, presentation_currency, company_currency, date)
 
-            if entry.get("debit"):
-                entry["debit"] = converted_debit_value
-            if entry.get("credit"):
-                entry["credit"] = converted_credit_value
+#             if entry.get("debit"):
+#                 entry["debit"] = converted_debit_value
+#             if entry.get("credit"):
+#                 entry["credit"] = converted_credit_value
 
-        converted_gl_list.append(entry)
+#         converted_gl_list.append(entry)
 
-    return converted_gl_list
+#     return converted_gl_list
+
+def custom_convert_to_presentation_currency(gl_entries, currency_info, filters=None):
+	converted_gl_list = []
+	presentation_currency = currency_info["presentation_currency"]
+	company_currency = currency_info["company_currency"]
+
+	account_currencies = list(set(entry["account_currency"] for entry in gl_entries))
+	exchange_gain_or_loss = False
+
+	if filters and isinstance(filters.get("account"), list):
+		account_filter = filters.get("account")
+		gain_loss_account = frappe.db.get_value("Company", filters.company, "exchange_gain_loss_account")
+
+		exchange_gain_or_loss = len(account_filter) == 1 and account_filter[0] == gain_loss_account
+
+	for entry in gl_entries:
+		debit = flt(entry["debit"])
+		credit = flt(entry["credit"])
+		debit_in_account_currency = flt(entry["debit_in_account_currency"])
+		credit_in_account_currency = flt(entry["credit_in_account_currency"])
+		account_currency = entry["account_currency"]
+
+		if (
+			len(account_currencies) == 1
+			and account_currency == presentation_currency
+			and not exchange_gain_or_loss
+		) and not (filters and filters.get("show_amount_in_company_currency")):
+			entry["debit"] = debit_in_account_currency
+			entry["credit"] = credit_in_account_currency
+		else:
+			date = entry.get("posting_date") or currency_info["report_date"]
+			converted_debit_value = convert(debit, presentation_currency, company_currency, date)
+			converted_credit_value = convert(credit, presentation_currency, company_currency, date)
+
+			if entry.get("debit"):
+				entry["debit"] = converted_debit_value
+
+			if entry.get("credit"):
+				entry["credit"] = converted_credit_value
+
+		converted_gl_list.append(entry)
+
+	return converted_gl_list
