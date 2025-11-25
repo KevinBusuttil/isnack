8ce09c48eccd6228efa6d80f15e7e91368b2e176
Kevin Busuttil <kevin.busuttil@busuttil-technologies.com>
2025-11-25 21:31:21 +0100

    Factory Settings

---
+++ isnack/isnack/doctype/label_template/test_label_template.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, Busuttil Technologies Limited and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestLabelTemplate(FrappeTestCase):
+	pass
