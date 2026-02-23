1e5bd29280770adbf23615c02edd82ecc00e7f0e
Kevin Busuttil <kevin.busuttil@busuttil-technologies.com>
2026-02-23 20:51:23 +0100

    Refactor make_work_order function to remove confirmation flow and streamline work order creation

--- isnack/public/js/production_plan.js
+++ isnack/public/js/production_plan.js
@@ -1,12 +1,6 @@
 frappe.ui.form.on("Production Plan", {
-  refresh(frm) {
-    // Override the core handler so only our confirmation flow runs
-    frm.events.make_work_order = () => {
-      frm.events._confirm_and_make_work_order(frm);
-    };
-  },
-
-  _confirm_and_make_work_order(frm) {
+  make_work_order(frm) {
+    // Guard: if not submitted, just run directly
     if (!frm.doc.name || frm.doc.docstatus !== 1) {
       return frm.events._run_make_work_order(frm);
     }
